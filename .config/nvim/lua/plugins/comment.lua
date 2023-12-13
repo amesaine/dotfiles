13350bf753f7ab22ce7c828ed31b2f290c042010
jnzigg <jnzig@proton.me>
2023-12-14 00:32:24 +0800

    feat: comment.nvim

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,15 @@
+return {
+    'numToStr/Comment.nvim',
+    opts = {
+        toggler = {
+            line = 'mcc',
+            block = 'mbc',
+        },
+        opleader = {
+            line = 'mc',
+            block = 'mb',
+        },
+    },
+    lazy = false,
+}
+
